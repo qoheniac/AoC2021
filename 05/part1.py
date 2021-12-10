9664ee6ce8db78df0315d19f3c6409ae5af48d82
qoheniac <29815186+qoheniac@users.noreply.github.com>
2021-12-11 00:25:15 +0100

    Day 5 - Part 2

---
+++ 05/part1.py
@@ -0,0 +1,27 @@
+from numpy import array, zeros
+
+# read input
+with open("input") as f:
+    lines = array(
+        [
+            [
+                [int(num) for num in coord.split(",")]
+                for coord in row.split(" -> ")
+            ]
+            for row in f.read().rstrip().split("\n")
+        ]
+    )
+
+# init overlaps matrix
+x_max, y_max = lines.reshape(2 * len(lines), 2).max(axis=0)
+overlaps = zeros((y_max, x_max))
+
+# count overlaps of horizontal and vertical lines
+for [x1, y1], [x2, y2] in lines:
+    if x1 == x2:
+        top, bottom = sorted([y1, y2])
+        overlaps[top : bottom + 1, x1] += 1
+    elif y1 == y2:
+        left, right = sorted([x1, x2])
+        overlaps[y1, left : right + 1] += 1
+print((overlaps >= 2).sum())
