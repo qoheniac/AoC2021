477d8598642e7cf231fcf726a003c7e1aad74e8e
qoheniac <29815186+qoheniac@users.noreply.github.com>
2021-12-24 22:55:53 +0100

    add comments

--- 19/part1.py
+++ 19/part1.py
@@ -1,3 +1,4 @@
+# read relative beacon positions
 with open("input") as f:
     data = [
         [
@@ -7,13 +8,17 @@ with open("input") as f:
         for scanner in f.read().split("\n\n")
     ]
 
+# initialization
 beacons = set(data[0])
 unlocated = set(range(1, len(data)))
 N = len(unlocated)
 
+# iterate unlocated scanners until all are located
 while len(unlocated) > 0:
     print(f"{round(100 * (1 - len(unlocated) / N)):3d}%", end="\r")
     for i in unlocated:
+
+        # iterate rotations
         for permutation in range(3):
             for parity in (1, -1):
                 for σ in [
@@ -22,6 +27,9 @@ while len(unlocated) > 0:
                     (-parity, parity, -parity),
                     (-parity, -parity, parity)
                 ]:
+
+                    # calculate differences between relative beacon locations
+                    # and known absolute beacon locations and count occurrences
                     count = dict()
                     for abs_beacon in beacons:
                         for rel_beacon in data[i]:
@@ -34,6 +42,11 @@ while len(unlocated) > 0:
                                 abs_beacon[i] - beacon[i] for i in range(3)
                             )
                             count[Δ] = count[Δ] + 1 if Δ in count else 1
+
+                            # if occurs for twelfth time calculate absolute
+                            # beacon positions and continue with an yet
+                            # unlocated scanner if one is left, else print
+                            # total number of beacons
                             if count[Δ] == 12:
                                 break
                         else:
--- 19/part2.py
+++ 19/part2.py
@@ -1,3 +1,4 @@
+# read relative beacon positions
 with open("input") as f:
     data = [
         [
@@ -7,14 +8,18 @@ with open("input") as f:
         for scanner in f.read().split("\n\n")
     ]
 
+# initialization
 scanners = [(0, 0, 0)]
 beacons = set(data[0])
 unlocated = set(range(1, len(data)))
 N = len(unlocated)
 
+# iterate unlocated scanners until all are located
 while len(unlocated) > 0:
     print(f"{round(100 * (1 - len(unlocated) / N)):3d}%", end="\r")
     for i in unlocated:
+
+        # iterate rotations
         for permutation in range(3):
             for parity in (1, -1):
                 for σ in [
@@ -23,6 +28,9 @@ while len(unlocated) > 0:
                     (-parity, parity, -parity),
                     (-parity, -parity, parity),
                 ]:
+
+                    # calculate differences between relative beacon locations
+                    # and known absolute beacon locations and count occurrences
                     count = dict()
                     for abs_beacon in beacons:
                         for rel_beacon in data[i]:
@@ -35,6 +43,10 @@ while len(unlocated) > 0:
                                 abs_beacon[i] - beacon[i] for i in range(3)
                             )
                             count[Δ] = count[Δ] + 1 if Δ in count else 1
+
+                            # if occurs for twelfth time save scanner position,
+                            # calculate absolute beacon positions and continue
+                            # with an yet unlocated scanner if one is left
                             if count[Δ] == 12:
                                 break
                         else:
@@ -61,9 +73,10 @@ while len(unlocated) > 0:
         else:
             continue
         break
-largest_distance = 0
 
+# find and print largest Manhattan distance between scanners
 N = len(scanners)
+largest_distance = 0
 for i in range(N - 1):
     for j in range(i, N):
         largest_distance = max(
