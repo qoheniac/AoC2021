3593b4494f21aac3140991a6e03901e41a4536a8
qoheniac <29815186+qoheniac@users.noreply.github.com>
2021-12-24 18:50:15 +0100

    fix sign error and remove unnecessary code (still takes nearly half a minute)

--- 19/part1.py
+++ 19/part1.py
@@ -7,20 +7,21 @@ with open("input") as f:
         for scanner in f.read().split("\n\n")
     ]
 
-signals = set([(0, 0, 0)])
 beacons = set(data[0])
 unlocated = set(range(1, len(data)))
+N = len(unlocated)
 
 while len(unlocated) > 0:
-    print(unlocated)
+    print(f"{round(100 * (1 - len(unlocated) / N)):3d}%", end="\r")
     for i in unlocated:
         for permutation in range(3):
             for parity in (1, -1):
                 for σ in [
-                    tuple(parity * (1 if m == n else -1) for m in range(3))
-                    for n in range(4)
+                    (parity, parity, parity),
+                    (parity, -parity, -parity),
+                    (-parity, parity, -parity),
+                    (-parity, -parity, parity)
                 ]:
-                    # Δs = []
                     count = dict()
                     for abs_beacon in beacons:
                         for rel_beacon in data[i]:
@@ -32,28 +33,14 @@ while len(unlocated) > 0:
                             Δ = tuple(
                                 abs_beacon[i] - beacon[i] for i in range(3)
                             )
-                            if Δ in count and count[Δ] > 0:
-                                count[Δ] += 1
-                                if count[Δ] >= 12:
-                                    break
-                            else:
-                                count[Δ] = 0
-                                for signal in signals:
-                                    if (
-                                        max(
-                                            abs(signal[i] - Δ[i])
-                                            for i in range(3)
-                                        )
-                                        < 2000
-                                    ):
-                                        count[Δ] = 1
-                                        break
+                            count[Δ] = count[Δ] + 1 if Δ in count else 1
+                            if count[Δ] == 12:
+                                break
                         else:
                             continue
                         break
                     else:
                         continue
-                    signals.add(Δ)
                     for rel_beacon in data[i]:
                         beacon = (
                             σ[0] * rel_beacon[permutation],
@@ -72,4 +59,4 @@ while len(unlocated) > 0:
         else:
             continue
         break
-print(len(beacons))
+print(f"    \r{len(beacons)}")
